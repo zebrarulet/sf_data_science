cfa40880902187479bb7c91958d82a7699f3bbc5
zebra <zebra.rulet@gmail.com>
2025-02-23 16:04:59 +0300

    add requirements.txt

--- guess-number-task/game_v3.py
+++ guess-number-task/game_v3.py
@@ -1,8 +1,28 @@
 """Игра угадай число
 Компьютер сам загадывает и сам угадывает число
+Усовершенствованная вверсия игры game_v2.py
 """
 
 import numpy as np
+
+def score_game(random_predict) -> int:
+    """За какое количество попыток в среднем за 1000 подходов угадывает наш алгоритм
+
+    Args:
+        random_predict ([type]): функция угадывания
+
+    Returns:
+        int: среднее количество попыток
+    """
+    count_ls = []
+    #np.random.seed(1)  # фиксируем сид для воспроизводимости
+    random_array = np.random.randint(1, 101, size=(1000))  # загадали список чисел
+
+    for number in random_array:
+        count_ls.append(random_predict(number))
+
+    score = int(np.mean(count_ls))
+    print(f"Ваш алгоритм угадывает число в среднем за: {score} попытки")
 
 def game_core_v3(number: int = 1) -> int:
     """
@@ -13,7 +33,6 @@ def game_core_v3(number: int = 1) -> int:
         int: Число попыток
     """
     # Ваш код начинается здесь
-
     count = 0
     low = 1
     high = 100
@@ -26,11 +45,8 @@ def game_core_v3(number: int = 1) -> int:
         elif number < predict:
             high = predict - 1
         predict = (low + high) // 2
-
     # Ваш код заканчивается здесь
 
     return count
 
-if __name__ == "__main__":
-    # RUN
-    game_core_v3(random_predict)
+score_game(game_core_v3)
